3f44905c33471692854b168dfff475cdbb0f11c3
fy <fy0@qq.com>
2020-03-14 18:21:47 +0800

    fix

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -74,28 +74,38 @@ export class SlimSQLAPI extends SlimBaseAPI {
   }
 
   /**
-   * 更新数据
+   * 更新数据，当存在 returning 时，返回影响的数据列表，否则返回影响数据的个数
    * @param params 查询参数，规则可参考 https://fy0.github.io/slim/#/quickstart/query_and_modify
    * @param data 赋值参数，规则参考同一页面
-   * @param param2 附加可选信息，其中 bulk 是一个批量标记，当存在时，此次调用会影响多个结果
+   * @param param2 附加可选信息，其中 bulk 是一个批量标记，当存在时，此次调用会影响多个结果。returning为true则返回受影响的数据列表
    */
-  async set (params, data, { bulk = undefined, role = sentinel }) {
-    return this.request('/update', 'POST', { params, data, bulk, role })
+  async set (params, data, { bulk = undefined, role = sentinel, returning }) {
+    return this.request('/update', 'POST', { params, data, bulk, role, returning })
   }
 
   /**
-   * 
+   * 新建数据
    * @param data 赋值参数，规则参考 https://fy0.github.io/slim/#/quickstart/query_and_modify
-   * @param param1 
+   * @param param1 附加可选信息，returning为true返回新建的数据记录
    */
-  async new (data, { role = sentinel }) {
-    return this.request('/new', 'POST', { data, role })
+  async new (data, { role = sentinel, returning }) {
+    return this.request('/new', 'POST', { data, role, returning })
   }
 
-  async bulkInsert (dataList, { role = sentinel }) {
-    return this.request('/bulk_insert', 'POST', { data: dataList, role })
+  /**
+   * 批量新建
+   * @param dataList 复数的数据记录
+   * @param param1 附加可选信息，returning为true返回新建的数据记录列表
+   */
+  async bulkInsert (dataList, { role = sentinel, returning }) {
+    return this.request('/bulk_insert', 'POST', { data: dataList, role, returning })
   }
 
+  /**
+   * 删除数据
+   * @param params 查询参数，规则可参考 https://fy0.github.io/slim/#/quickstart/query_and_modify
+   * @param param1 附加可选信息，其中 bulk 是一个批量标记，当存在时，此次调用会影响多个结果。
+   */
   async delete (params, { bulk = false, role = sentinel }) {
     return this.request('/delete', 'POST', { params, bulk, role })
   }
